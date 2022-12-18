b3ec6f1204e415c3595aa566606d561e6cda92ad
tetianavinnik <92581380+tetianavinnik@users.noreply.github.com>
2022-12-18 19:55:48 +0200

    Add files via upload

---
+++ src/main/java/visitor/Signature.java
@@ -0,0 +1,22 @@
+package visitor;
+
+
+import java.util.UUID;
+import java.util.function.Consumer;
+
+public class Signature<T> extends Task<T> {
+    public Consumer<T> consumer;
+    public Signature(Consumer<T> consumer) {
+        this.consumer = consumer;
+    }
+
+    public void apply(T arg) {
+        this.freeze();
+        consumer.accept(arg);
+    }
+
+    @Override
+    public void stamp(Visitor<T> visitor) {
+        this.setHeader("groups", visitor.onSignature(this).get("groups"));
+    }
+}
